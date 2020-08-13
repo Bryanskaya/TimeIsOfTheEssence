d8f36dcadeb9b3090edf6e21cbdfb7de7d8adb5d
Bryanskaya <bryanskayakatya@yandex.ru>
2020-08-13 19:47:06 +0300

    CORRECTED: transform commands

--- Coursework/coursework/Commands/transform_command.h
+++ Coursework/coursework/Commands/transform_command.h
@@ -2,34 +2,39 @@
 #define TRANSFORM_COMMAND_H
 
 #include "base_command.h"
+#include "Transformation/transformator.h"
+
 
 class TransformCommand : public BaseCommand
 {
 public:
-    //TransformCommand(Transformation* transf, size_t index);
+    TransformCommand(const shared_ptr<Transformator>& transf);
     virtual ~TransformCommand() = 0;
 
-    virtual void execute(weak_ptr<Scene> scene);
+    virtual void execute(weak_ptr<Scene> scene) = 0;
 
 protected:
-    //shared_ptr<Transformator> _transf;
-    size_t _index;
+    shared_ptr<Transformator> _transf;
 };
 
 
 class RotateCommand : public TransformCommand
 {
 public:
-    //RotateCommand(double ax, double ay, double az, size_t index);
+    RotateCommand(const Point& center, const Vector& vect);
     virtual ~RotateCommand() = 0;
+
+    virtual void execute(weak_ptr<Scene> scene) = 0;
 };
 
 
 class MoveCommand : public TransformCommand
 {
 public:
-    //MoveCommand(double dx, double dy, double dz, size_t index);
+    MoveCommand(const Vector& vect);
     virtual ~MoveCommand() = 0;
+
+    virtual void execute(weak_ptr<Scene> scene) = 0;
 };
 
 
@@ -37,32 +42,40 @@ public:
 class RotateCamera : public RotateCommand
 {
 public:
-    //RotateCamera(Vector move);
+     RotateCamera(const Vector& vect);
      virtual ~RotateCamera();
+
+     virtual void execute(weak_ptr<Scene> scene);
 };
 
 
 class RotateLightSource : public RotateCommand
 {
 public:
-    //RotateLightSource(Vector move);
-     virtual ~RotateLightSource();
+    RotateLightSource(const Point& center, const Vector& vect);
+    virtual ~RotateLightSource();
+
+    virtual void execute(weak_ptr<Scene> scene);
 };
 
 
 class MoveCamera : public MoveCommand
 {
 public:
-    //MoveCamera(Vector move);
-     virtual ~MoveCamera();
+    MoveCamera(const Vector& vect);
+    virtual ~MoveCamera();
+
+    virtual void execute(weak_ptr<Scene> scene);
 };
 
 
 class MoveLightSource : public MoveCommand
 {
 public:
-    //MoveLightSource(Vector move);
-     virtual ~MoveLightSource();
+    MoveLightSource(const Vector& vect);
+    virtual ~MoveLightSource();
+
+    virtual void execute(weak_ptr<Scene> scene);
 };
 
 
