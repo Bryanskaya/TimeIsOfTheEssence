edfc8d7aadc24f97554f163e98fab26ff1192efc
Bryanskaya <bryanskayakatya@yandex.ru>
2020-08-15 21:32:54 +0300

    ADD: new class Box

---
+++ Coursework/coursework/Primitives/Box/box.cpp
@@ -0,0 +1,39 @@
+#include "box.h"
+
+Box::Box(const Point& pnt1, const Point& pnt2, QRgb color) :
+    _color(color)
+{
+    //Нижняя грань
+    add_vertex(pnt1);   //Ближайшая к наблюдателю слева снизу
+    add_vertex(Point(pnt1.x, pnt1.y, pnt2.z));
+    add_vertex(Point(pnt2.x, pnt1.y, pnt2.z));
+    add_vertex(Point(pnt2.x, pnt1.y, pnt1.z));
+
+    //Верхняя грань
+    add_vertex(Point(pnt1.x, pnt2.y, pnt1.z));
+    add_vertex(Point(pnt1.x, pnt2.y, pnt2.z));
+    add_vertex(pnt2);
+    add_vertex(Point(pnt2.x, pnt2.y, pnt1.z));
+
+
+    add_side({0, 1, 2, 3}, color);  //Нижняя грань
+    add_side({4, 5, 6, 7}, color);  //Верхняя грань
+
+    add_side({0, 4, 7, 3}, color);  //Лицевая
+    add_side({0, 4, 5, 1}, color);    //Левая
+    add_side({1, 5, 6, 2}, color);    //Задняя
+    add_side({3, 7, 6, 2}, color);    //Правая
+
+    normalize_n_vrt();
+    _find_center(pnt1, pnt2);
+}
+
+Box::~Box() {}
+
+
+void Box::_find_center(const Point &pnt1, const Point &pnt2)
+{
+    _center.x = (pnt1.x + pnt2.x) / 2;
+    _center.y = (pnt1.y + pnt2.y) / 2;
+    _center.z = (pnt1.z + pnt2.z) / 2;
+}
--- Coursework/coursework/Primitives/Box/box.h
+++ Coursework/coursework/Primitives/Box/box.h
@@ -1,4 +1,21 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef BOX_H
+#define BOX_H
 
-#endif // MAINWINDOW_H
+#include "Primitives/Model/model.h"
+
+class Box : public Model
+{
+public:
+    Box(const Point& pnt1, const Point& pnt2, QRgb color);
+
+    virtual ~Box();
+
+private:
+    QRgb _color;
+
+    void _find_center(const Point& pnt1, const Point& pnt2);
+};
+
+
+
+#endif // BOX_H
