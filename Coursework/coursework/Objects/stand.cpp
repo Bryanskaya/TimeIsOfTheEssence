d64ded679c5e428749ecace0e46f2e55cbb23af8
Bryanskaya <bryanskayakatya@yandex.ru>
2020-08-15 21:31:42 +0300

    ADD: object stand

---
+++ Coursework/coursework/Objects/stand.cpp
@@ -0,0 +1,34 @@
+#include "stand.h"
+#include "Visitors/object_visitor.h"
+
+
+//constructors and destructor
+Stand::Stand(double x, double y, double z)
+{
+    _color = QColor(Qt::darkRed).rgba();
+
+    Point pnt1(x, y, z);
+    Point pnt2(x + _length, y + _height, z - _length);
+
+    Model* model_ptr = new Box(pnt1, pnt2, _color);
+    _model = shared_ptr<Model>(model_ptr);
+}
+
+Stand::Stand(const Stand& other) : VisibleObject(other)
+{
+    _color = other._color;
+}
+
+Stand::~Stand() {}
+
+
+//methonds
+void Stand::accept(ObjectVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+SceneObject* Stand::clone()
+{
+    return new Stand(*this);
+}
--- Coursework/coursework/Objects/stand.h
+++ Coursework/coursework/Objects/stand.h
@@ -1,4 +1,25 @@
 #ifndef STAND_H
 #define STAND_H
 
+#include "simple_object.h"
+#include "Primitives/Box/box.h"
+
+
+class Stand : public VisibleObject
+{
+public:
+    Stand(double x, double y, double z);
+    explicit Stand(const Stand& other);
+
+    virtual ~Stand();
+
+    virtual void accept(ObjectVisitor& visitor);
+    virtual SceneObject* clone();
+
+private:
+    QRgb _color;
+    const double _height = 20;
+    const double _length = 240;
+};
+
 #endif // STAND_H
