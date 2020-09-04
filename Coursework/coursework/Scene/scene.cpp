c7b471acb30ad9c86da776982caf9486b431e7ef
Bryanskaya <bryanskayakatya@yandex.ru>
2020-09-04 19:58:27 +0300

    CORRECTED: camera position

--- Coursework/coursework/Scene/scene.cpp
+++ Coursework/coursework/Scene/scene.cpp
@@ -3,7 +3,7 @@
 Scene::Scene()
 {
     set_camera(Camera(Point(300, 200, 500), Vector(0, 0, 0)));
-    set_light(LightSource(Point(0, 1000, 1000), 1));
+    set_light(LightSource(Point(300, 700, -10), 1));
     add_object(new Stand(170, 100, 120));
     add_object(new Stand(170, 500, 120));
     //add_object(new Hourglass());
