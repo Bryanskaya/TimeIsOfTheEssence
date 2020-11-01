5ac9fc716666b6224c4ac86502201d2194451535
Bryanskaya <bryanskayakatya@yandex.ru>
2020-11-01 13:05:59 +0300

    CORRECTED: some details

--- Coursework/build-coursework-Desktop_Qt_MinGW_w64_32bit_MSYS2-Debug/debug/moc_mainwindow.cpp
+++ Coursework/build-coursework-Desktop_Qt_MinGW_w64_32bit_MSYS2-Debug/debug/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[7];
-    char stringdata0[108];
+    QByteArrayData data[11];
+    char stringdata0[178];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,12 +37,18 @@ QT_MOC_LITERAL(2, 29, 0), // ""
 QT_MOC_LITERAL(3, 30, 20), // "on_PushRight_clicked"
 QT_MOC_LITERAL(4, 51, 19), // "on_PushDown_clicked"
 QT_MOC_LITERAL(5, 71, 19), // "on_PushLeft_clicked"
-QT_MOC_LITERAL(6, 91, 16) // "on_Start_clicked"
+QT_MOC_LITERAL(6, 91, 16), // "on_Start_clicked"
+QT_MOC_LITERAL(7, 108, 21), // "on_PushCloser_clicked"
+QT_MOC_LITERAL(8, 130, 22), // "on_PushFurther_clicked"
+QT_MOC_LITERAL(9, 153, 13), // "keyPressEvent"
+QT_MOC_LITERAL(10, 167, 10) // "QKeyEvent*"
 
     },
     "MainWindow\0on_PushUp_clicked\0\0"
     "on_PushRight_clicked\0on_PushDown_clicked\0"
-    "on_PushLeft_clicked\0on_Start_clicked"
+    "on_PushLeft_clicked\0on_Start_clicked\0"
+    "on_PushCloser_clicked\0on_PushFurther_clicked\0"
+    "keyPressEvent\0QKeyEvent*"
 };
 #undef QT_MOC_LITERAL
 
@@ -52,7 +58,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -60,11 +66,14 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x08 /* Private */,
-       3,    0,   40,    2, 0x08 /* Private */,
-       4,    0,   41,    2, 0x08 /* Private */,
-       5,    0,   42,    2, 0x08 /* Private */,
-       6,    0,   43,    2, 0x08 /* Private */,
+       1,    0,   54,    2, 0x08 /* Private */,
+       3,    0,   55,    2, 0x08 /* Private */,
+       4,    0,   56,    2, 0x08 /* Private */,
+       5,    0,   57,    2, 0x08 /* Private */,
+       6,    0,   58,    2, 0x08 /* Private */,
+       7,    0,   59,    2, 0x08 /* Private */,
+       8,    0,   60,    2, 0x08 /* Private */,
+       9,    1,   61,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -72,6 +81,9 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 10,    2,
 
        0        // eod
 };
@@ -87,10 +99,12 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 2: _t->on_PushDown_clicked(); break;
         case 3: _t->on_PushLeft_clicked(); break;
         case 4: _t->on_Start_clicked(); break;
+        case 5: _t->on_PushCloser_clicked(); break;
+        case 6: _t->on_PushFurther_clicked(); break;
+        case 7: _t->keyPressEvent((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
@@ -122,13 +136,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 8;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 8)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 8;
     }
     return _id;
 }
