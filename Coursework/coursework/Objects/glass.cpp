648e0d746055c2fc38d35157e7bca407db14fc6c
Bryanskaya <bryanskayakatya@yandex.ru>
2020-12-05 15:25:48 +0300

    ADDED: glass

--- Coursework/coursework/Objects/glass.cpp
+++ Coursework/coursework/Objects/glass.cpp
@@ -1,18 +1,31 @@
 #include "glass.h"
+#include "Visitors/object_visitor.h"
+
 
 //constructors and destructor
-Glass::Glass() {}
+Glass::Glass(const Point& pnt1, const Point& pnt2)
+{
+    _color = QColor(Qt::lightGray).rgba();
+
+    Model* model_ptr = new Piramid(pnt1, pnt2, _color);
+    _model = shared_ptr<Model>(model_ptr);
+}
+
+Glass::Glass(const Glass& other) : VisibleObject(other)
+{
+    _color = other._color;
+}
 
 Glass::~Glass() {}
 
 
 //methonds
-void Glass::accept(ObjectVisitor& visitor)
+void Glass::accept(ObjectVisitor &visitor)
 {
-    //visitor.visit(*this);
+    visitor.visit(*this);
 }
 
-SceneObject *Glass::clone()
+SceneObject* Glass::clone()
 {
     return new Glass(*this);
 }
--- Coursework/coursework/Objects/glass.h
+++ Coursework/coursework/Objects/glass.h
@@ -2,21 +2,24 @@
 #define GLASS_H
 
 #include "simple_object.h"
+#include "Primitives/Piramid/piramid.h"
 
 
 class Glass : public VisibleObject
 {
 public:
-    Model model;
-    double k_ref;
-    double k_art;
-
-    Glass();
+    Glass(const Point& pnt1, const Point& pnt2);
+    explicit Glass(const Glass& other);
 
     virtual ~Glass();
 
-    virtual void accept(ObjectVisitor&);
+    virtual void accept(ObjectVisitor& visitor);
     virtual SceneObject* clone();
+
+private:
+    QRgb _color;
+
+    double k;
 };
 
 #endif // GLASS_H
