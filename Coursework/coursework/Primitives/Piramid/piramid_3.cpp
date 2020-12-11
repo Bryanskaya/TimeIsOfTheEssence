46ffcdf30ae44a2389c3032e7abce3c27e2a34ba
Bryanskaya <bryanskayakatya@yandex.ru>
2020-12-11 22:08:17 +0300

    ADDED: piramid of 3 points

---
+++ Coursework/coursework/Primitives/Piramid/piramid_3.cpp
@@ -0,0 +1,26 @@
+#include "piramid_3.h"
+
+#include <iostream>
+using namespace std;
+
+Piramid_3::Piramid_3(const Point& pnt1, const Point& pnt2, const Point& pnt3, const Point& pnt_top, QRgb color) :
+    _color(color)
+{
+    _center = Point((pnt1.x + pnt2.x + pnt3.x + pnt_top.x) / 4,
+                    (pnt1.y + pnt2.y + pnt3.y + pnt_top.y) / 4,
+                    (pnt1.z + pnt2.z + pnt3.z + pnt_top.z) / 4);
+
+    add_vertex(pnt1);
+    add_vertex(pnt2);
+    add_vertex(pnt3);
+    add_vertex(pnt_top);
+
+    add_side({1, 2, 3}, color);  // Правая боковая грань
+    add_side({2, 0, 3}, color);  // Левая боковая грань
+    add_side({0, 1, 3}, color);  // Лицевая грань
+    add_side({0, 1, 2}, color);  // Дно
+
+    normalize_n_vrt();
+}
+
+Piramid_3::~Piramid_3() {}
--- Coursework/coursework/Primitives/Piramid/piramid_3.h
+++ Coursework/coursework/Primitives/Piramid/piramid_3.h
@@ -1,4 +1,17 @@
 #ifndef PIRAMID_3_H
 #define PIRAMID_3_H
 
+#include "Primitives/Model/model.h"
+
+class Piramid_3 : public Model
+{
+public:
+    Piramid_3(const Point& pnt1, const Point& pnt2, const Point& pnt3, const Point& pnt_top, QRgb color);
+
+    virtual ~Piramid_3();
+
+private:
+    QRgb _color;
+};
+
 #endif // PIRAMID_3_H
