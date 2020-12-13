72e7dc46b753f5babb55038442b24c3c163881ee
Bryanskaya <bryanskayakatya@yandex.ru>
2020-12-13 16:12:29 +0300

    CORRECTED: update visitor

--- Coursework/coursework/Objects/Visitors/update_visitor.cpp
+++ Coursework/coursework/Objects/Visitors/update_visitor.cpp
@@ -43,29 +43,4 @@ void UpdateVisitor::visit(SandItem& item)
 
 void UpdateVisitor::visit(DemoBox&) {}
 
-/*void UpdateVisitor::move_sand_items()
-{
-    double step = 5; // что-то с этим придумать
-    for (size_t i = 0; i < массив_песчинок.size(); i++)
-    {
-        // обдумать обработку ошибки выхода
-        _move_sand_item(массив_песчинок[i], step);
-
-        //удаление в случае не пустого массива/ошибки
-    }
-}*/
-
-/*void UpdateVisitor::_move_sand_item(shared_ptr<SandItem> &item, double step)
-{
-    vector<shared_ptr<Vertex>>& v = item->get_model()->v_arr; //&???
-
-    for (size_t i = 0; i < v.size(); i++)
-    {
-        v[i]->y += step;
-
-        if (v[i]->y > _border)
-            throw i; // можно просто return в принципе учесть что циел должен работать до конца!!!
-        /* можно массив индексов для удаления сделать*/
-    //}
-//}
 
