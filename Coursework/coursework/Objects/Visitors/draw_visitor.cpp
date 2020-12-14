f6758e92915b14edfb93900a6fb80ee6463e1e13
Bryanskaya <bryanskayakatya@yandex.ru>
2020-12-14 18:57:11 +0300

    DELETED: useless methods

--- Coursework/coursework/Objects/Visitors/draw_visitor.cpp
+++ Coursework/coursework/Objects/Visitors/draw_visitor.cpp
@@ -13,12 +13,10 @@ void DrawVisitor::visit(LightSource&) {}
 void DrawVisitor::visit(Stand &stand)
 {
     _visual->draw_model(*stand.get_model());
-   // cout << "drawing ended" << endl;
 }
 
 void DrawVisitor::visit(Glass &glass)
 {
-    //_visual->draw_model(*glass.get_model());
    _visual->draw_intensity(*glass.get_model(), glass.get_transparency());
 }
 
@@ -37,30 +35,3 @@ void DrawVisitor::visit(SandItem &item)
    _visual->draw_model(*item.get_model());
 }
 
-
-/*TransparencyVisitor::TransparencyVisitor(shared_ptr<Visualizer>& visual) :
-    _visual(visual) {}
-
-TransparencyVisitor::~TransparencyVisitor() {}
-
-void TransparencyVisitor::visit(Camera&) {}
-
-void TransparencyVisitor::visit(LightSource&) {}
-
-void TransparencyVisitor::visit(Hourglass &hourglass)
-{
-    //_visual->draw_model();
-}
-
-void TransparencyVisitor::visit(Stand &stand)
-{
-    //_visual->draw_intensity(*stand.get_model());
-   // cout << "drawing ended" << endl;
-}
-
-void TransparencyVisitor::visit(Glass &glass) // надо ли делать на само стекло
-{
-    cout << "hi\n";
-    _visual->draw_intensity(*glass.get_model(), glass.get_transparency());
-   // cout << "drawing ended" << endl;
-}*/
