f4e54a9eada7c908b8dc4b65a68654503f26bf57
Bryanskaya <bryanskayakatya@yandex.ru>
2020-12-14 17:55:06 +0300

    CORRECTED: some details

--- Coursework/coursework/Objects/Visitors/object_visitor.h
+++ Coursework/coursework/Objects/Visitors/object_visitor.h
@@ -3,10 +3,10 @@
 
 #include "Objects/camera.h"
 #include "Objects/light_source.h"
-#include "Objects/hourglass.h"
 #include "Objects/stand.h"
 #include "Objects/glass.h"
 #include "Objects/surface_object.h"
+#include "Objects/sand.h"
 
 
 class ObjectVisitor
@@ -18,15 +18,12 @@ public:
     virtual void visit(Camera& camera) = 0;
     virtual void visit(LightSource& light) = 0;
 
-    virtual void visit(Hourglass& hourglass) = 0;
     virtual void visit(Stand& stand) = 0;
     virtual void visit(Glass& glass) = 0;
 
     virtual void visit(SandUpP& sand) = 0;
     virtual void visit(SurfaceObject& sand) = 0;
     virtual void visit(SandItem& item) = 0;
-
-    virtual void visit(DemoBox& demobox) = 0;
 };
 
 #endif // OBJECT_VISITOR_H
