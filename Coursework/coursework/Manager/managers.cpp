6234ad3215512af5e19a6930f39521d9b9e3a09b
Bryanskaya <bryanskayakatya@yandex.ru>
2020-12-14 07:51:33 +0300

    CORRECTED: work of manager

--- Coursework/coursework/Manager/managers.cpp
+++ Coursework/coursework/Manager/managers.cpp
@@ -151,6 +151,5 @@ void InitStateManager::execute()
     double d_temp = 70 * temp;
 
     _scene.lock()->add_object(new SurfaceObject(Point(-d_temp, h_temp, d_temp), Point(d_temp, h_temp, -d_temp), Point(-1, 0, 1), true));
-    _scene.lock()->add_object(new SurfaceObject(Point(-70, -210, 70), Point(70, -210, -70)));
-
+    _scene.lock()->add_object(new SurfaceObject(Point(-115, -195, 115), Point(115, -195, -115)));
 }
