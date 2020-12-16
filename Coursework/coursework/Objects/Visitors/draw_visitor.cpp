74a91516f900e94f996c872448b16297fc70200f
Bryanskaya <bryanskayakatya@yandex.ru>
2020-12-16 07:35:16 +0300

    ADDED: some methods for work with shadows

--- Coursework/coursework/Objects/Visitors/draw_visitor.cpp
+++ Coursework/coursework/Objects/Visitors/draw_visitor.cpp
@@ -35,3 +35,38 @@ void DrawVisitor::visit(SandItem &item)
    _visual->draw_model(*item.get_model());
 }
 
+
+
+DrawShdVisitor::DrawShdVisitor(shared_ptr<Visualizer>& visual) :
+    _visual(visual) {}
+
+DrawShdVisitor::~DrawShdVisitor() {}
+
+void DrawShdVisitor::visit(Camera&) {}
+
+void DrawShdVisitor::visit(LightSource&) {}
+
+void DrawShdVisitor::visit(Stand &stand)
+{
+    _visual->draw_shadows(*stand.get_model());
+}
+
+void DrawShdVisitor::visit(Glass &glass)
+{
+   //_visual->draw_intensity(*glass.get_model(), glass.get_transparency());
+}
+
+void DrawShdVisitor::visit(SandUpP &sand)
+{
+   _visual->draw_shadows(*sand.get_model());
+}
+
+void DrawShdVisitor::visit(SurfaceObject &sand)
+{
+   _visual->draw_shadows(*sand.get_model());
+}
+
+void DrawShdVisitor::visit(SandItem &item)
+{
+   _visual->draw_shadows(*item.get_model());
+}
--- Coursework/coursework/Objects/Visitors/draw_visitor.h
+++ Coursework/coursework/Objects/Visitors/draw_visitor.h
@@ -25,4 +25,26 @@ private:
     shared_ptr<Visualizer> _visual;
 };
 
+
+
+class DrawShdVisitor : public ObjectVisitor
+{
+public:
+    DrawShdVisitor(shared_ptr<Visualizer>& visual);
+    virtual ~DrawShdVisitor();
+
+    virtual void visit(Camera& camera);
+    virtual void visit(LightSource& light);
+
+    virtual void visit(Stand& stand);
+
+    virtual void visit(Glass& glass);
+    virtual void visit(SandUpP& sand);
+    virtual void visit(SurfaceObject& sand);
+    virtual void visit(SandItem& item);
+
+private:
+    shared_ptr<Visualizer> _visual;
+};
+
 #endif // DRAW_VISITOR_H
