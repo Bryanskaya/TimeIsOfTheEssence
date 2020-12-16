ba4dc5a2790faa994ae4a197d89cc0d911cda660
Bryanskaya <bryanskayakatya@yandex.ru>
2020-12-16 07:36:24 +0300

    DELETED: creating of middle points in piramid

--- Coursework/coursework/Primitives/Piramid/piramid.cpp
+++ Coursework/coursework/Primitives/Piramid/piramid.cpp
@@ -13,10 +13,10 @@ Piramid::Piramid(const Point& pnt1, const Point& pnt2, QRgb color, double down_l
     add_vertex(Point(pnt1.x + down_length, pnt1.y, pnt1.z - down_length));
     add_vertex(Point(pnt1.x + down_length, pnt1.y, pnt1.z));
 
-    add_vertex(Point((pnt1.x + pnt2.x) / 2, (pnt1.y + pnt2.y) / 2, (pnt1.z + pnt2.z) / 2));
+    /*add_vertex(Point((pnt1.x + pnt2.x) / 2, (pnt1.y + pnt2.y) / 2, (pnt1.z + pnt2.z) / 2));
     add_vertex(Point((pnt1.x + pnt2.x) / 2, (pnt1.y + pnt2.y) / 2, (pnt1.z - down_length + pnt2.z - up_length) / 2));
     add_vertex(Point((pnt1.x + down_length + pnt2.x + up_length) / 2, (pnt1.y + pnt2.y) / 2, (pnt1.z - down_length + pnt2.z - up_length) / 2));
-    add_vertex(Point((pnt1.x + down_length + pnt2.x + up_length) / 2, (pnt1.y + pnt2.y) / 2, (pnt1.z + pnt2.z) / 2));
+    add_vertex(Point((pnt1.x + down_length + pnt2.x + up_length) / 2, (pnt1.y + pnt2.y) / 2, (pnt1.z + pnt2.z) / 2));*/
 
     add_vertex(pnt2);
     add_vertex(Point(pnt2.x, pnt2.y, pnt2.z - up_length));
@@ -28,10 +28,10 @@ Piramid::Piramid(const Point& pnt1, const Point& pnt2, QRgb color, double down_l
     add_side({2, 6, 7, 3}, color);  // Правая боковая грань
     add_side({0, 4, 7, 3}, color);  // Лицевая грань
 
-    add_side({5, 9, 10, 6}, color);
+    /*add_side({5, 9, 10, 6}, color);
     add_side({4, 8, 9, 5}, color);
     add_side({6, 10, 11, 7}, color);
-    add_side({4, 8, 11, 7}, color);
+    add_side({4, 8, 11, 7}, color);*/
 
 
     /*add_vertex(pnt1);
